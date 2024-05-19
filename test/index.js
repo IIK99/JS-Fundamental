8358615715c3d259b0897eb70827d89fc6dbddc2
IIK99 <muhamad.ikmal1099@gmail.com>
2024-05-19 20:03:34 +0700

    functions

--- test/index.js
+++ test/index.js
@@ -13,8 +13,8 @@ resultt = 18 + inputNumber;
 resultt = resultt - 5;
 resultt = resultt * 2;
 resultt = resultt / 3;
-console.log(resultt);
-console.log(inputNumber);
+console.log('result ',resultt);
+console.log('nilai awal ',inputNumber);
 
 function penjumlahan(a, b) {
   const hasilTambah = a + b;
@@ -30,3 +30,48 @@ function penjumlahan(a, b) {
 }
 
 penjumlahan(70, 20);
+console.log('break');
+penjumlahan(100, 20);
+
+function add(c, d) {
+    const hasilTambah = c + d;
+    return hasilTambah;
+}
+
+console.log('result add function',add(10, 20));
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function stringify(number) {
+    return "Result: " + number
+}
+
+let resultSubtract = subtract(12, 10)
+console.log('result subtract',resultSubtract);
+let resultstringify = stringify(10)
+console.log(resultstringify);
+
+function say (userName, message) {
+    console.log(userName, message);
+}
+
+say('John', 'Stephan');
+
+function say2 (userName, message = 'Hello') {
+    console.log(userName, message);
+} 
+say2('John')
+
+function createGreeting (userName, message = 'Hello') {
+    return "hi, i'm " + userName + '. ' + message;
+}
+
+console.log(createGreeting('John'));
+
+const greeting = createGreeting('Max')
+console.log(greeting);
+
+const greeting2 = createGreeting('Manuel', "Hello, what's up?");
+console.log(greeting2);
