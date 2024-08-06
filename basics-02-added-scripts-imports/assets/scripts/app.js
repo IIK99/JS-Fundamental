834f9c0de160c006dd53c639ab530c40570ca928
IIK99 <muhamad.ikmal1099@gmail.com>
2024-08-06 15:20:44 +0700

    update code

--- basics-02-added-scripts-imports/assets/scripts/app.js
+++
@@ -1,4 +0,0 @@
-let currentResult = 0
-currentResult = (currentResult + 10) * 3 / 2 - 1
-let calculationDescription = '(' + currentResult + ' + 10) * 3 / 2 - 1'
-outputResult(currentResult, calculationDescription) 
---
+++ basics-02-added-scripts-imports/assets/scripts/index.js
@@ -0,0 +1,5 @@
+let currentResult = 0;
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
+let calculationDescription = "(" + currentResult + " + 10) * 3 / 2 - 1";
+outputResult(currentResult, calculationDescription);
+console.log(currentResult, calculationDescription);
