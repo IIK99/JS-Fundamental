2ca763a56a4678f9f4a517f52939e39e71362e69
IIK99 <muhamad.ikmal1099@gmail.com>
2024-08-21 17:01:43 +0700

    update code

--- basics-02-added-scripts-imports/assets/scripts/app.js
+++ basics-02-added-scripts-imports/assets/scripts/app.js
@@ -1,5 +1,22 @@
-let currentResult = 0;
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
-let calculationDescription = "(" + currentResult + " + 10) * 3 / 2 - 1";
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function subtract(num1, num2) {
+  const result = num1 - num2;
+  alert("The result is" + result);
+}
+
+subtract(20, 7);
+
+function add(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
+
+currentResult = add(1, 2);
+
+// currentResult = ((currentResult + 10) * 3) / 2 - 1;
+
+let calculationDescription = `(${currentResult} + 10) * 3 / 2 - 1`;
+
 outputResult(currentResult, calculationDescription);
-console.log(currentResult, calculationDescription);
